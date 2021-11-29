e0233437e44deaab353217b60ffd4c4454fe588a
NatalliaMil <milkonatallia@gmail.com>
2021-11-29 10:59:37 +0300

    fixed additional component

--- newapp/src/app/additional/additional.component.ts
+++ newapp/src/app/additional/additional.component.ts
@@ -2,16 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-additional',
-  template: `<h2>Добро пожаловать, {{ name }}!</h2>
-    <p>{{ message }}</p>`,
-  styles: [
-    `
-      h2,
-      p {
-        color: blue;
-      }
-    `,
-  ],
+  templateUrl: './additional.component.html',
+  styleUrls: ['./additional.component.css'],
 })
 export class AdditionalComponent implements OnInit {
   constructor() {}
