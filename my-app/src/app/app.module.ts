80da777c55d4fb8869480b2f30cb3d940da0f7f6
NatalliaMil <milkonatallia@gmail.com>
2021-12-14 22:28:39 +0300

    Improve weather-app variant #2

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { ShowWeatherComponent } from './show-weather/show-weather.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ShowWeatherComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ my-app/src/app/show-weather/show-weather.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { WeatherService } from '../weather.service';
+
+@Component({
+  selector: 'app-show-weather',
+  templateUrl: './show-weather.component.html',
+  styleUrls: ['./show-weather.component.css'],
+})
+export class ShowWeatherComponent implements OnInit {
+  weather = 0;
+  @Input() weatherForm = false;
+
+  constructor(public weatherservice: WeatherService) {}
+
+  ngOnInit(): void {
+    this.weatherservice.weather.subscribe((value) => {
+      this.weather = value;
+    });
+  }
+}
