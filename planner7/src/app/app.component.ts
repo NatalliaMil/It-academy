fb7e79a92e84bac681d6ba0e7d6279775705ab15
NatalliaMil <milkonatallia@gmail.com>
2021-12-16 13:23:32 +0300

    implementation event.stopPropagation()

--- planner7/src/app/app.component.ts
+++ planner7/src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'planner7';
+
+  clikParent() {
+    console.log('Parent div click');
+  }
+
+  clikChild(event: any) {
+    console.log('Child div click');
+    // event.stopPropagation();
+  }
 }
