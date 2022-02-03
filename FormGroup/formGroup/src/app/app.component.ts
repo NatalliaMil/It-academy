f45fba921432e6dda325f0d1eb8babfbabf6743f
NatalliaMil <milkonatallia@gmail.com>
2022-02-03 18:28:24 +0300

    add formGroup

--- FormGroup/formGroup/src/app/app.component.ts
+++ FormGroup/formGroup/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'formGroup';
+  formData = {};
+  formDataTrue = false;
+  questionnaire = new FormGroup({
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    age: new FormControl('', [Validators.required]),
+    workPlace: new FormArray([new FormControl('', [Validators.required])]),
+  });
+  get workPlace() {
+    return this.questionnaire.get('workPlace') as FormArray;
+  }
+  addWorkPlace() {
+    this.workPlace.push(new FormControl('', [Validators.required]));
+  }
+  submit() {
+    this.formData = JSON.stringify(this.questionnaire.value);
+    this.formDataTrue = true;
+  }
 }
